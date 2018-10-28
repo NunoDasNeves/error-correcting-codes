c0665cc0b6b111dc8cd0770111422860f301170f
Nuno Das Neves <ndasneves@gmail.com>
2018-10-28 20:47:01 +1100

    basic encoder (not tested), encoding and decoding for string to binary

--- make.sh
+++ make.sh
@@ -1,5 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-tsc $1 --outFile out.js --target ES5;
-node out.js
+file=$(echo $1 | sed -r 's/\.ts$//g')
+
+if [[ "$file" == "$1" ]]; then
+    echo "Error: $1 should have .ts extension";
+    exit 1;
+fi
+
+tsc $1 --outFile out.js --target ES5 --lib es6,dom --noImplicitAny && node out.js
 
