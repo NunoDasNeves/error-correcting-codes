72483421d646af9fc338f66d04dccfc8f9f0bcbd
Nuno Das Neves <ndasneves@gmail.com>
2018-10-31 18:20:04 +1100

    mathjax working, using git submodule

---
+++ app/src/components/AppMathJax.vue
@@ -0,0 +1,48 @@
+<template>
+  <span ref="mathJaxEl">{{ data }}</span>
+</template>
+
+<script type="text/javascript">
+  export default {
+    props:['data'],
+
+    watch: {
+      data () {
+        this.renderMathJax()
+      }
+    },
+
+    methods: {
+      renderMathJax () {
+        if (window.MathJax) {
+          window.MathJax.Hub.Config({
+            extensions: ["tex2jax.js"],
+            jax: ["input/TeX", "output/HTML-CSS"],
+            tex2jax: {
+              inlineMath: [['$', '$'], ['(', ')']],
+              displayMath: [['$$', '$$'], ['[', ']']],
+              processEscapes: true,
+              processEnvironments: true
+            },
+            // Center justify equations in code and markdown cells. Elsewhere
+            // we use CSS to left justify single line equations in code cells.
+            displayAlign: 'center',
+            'HTML-CSS': {
+              fonts: ['TeX'],
+              styles: { '.MathJax_Display': { margin: 0 } },
+              linebreaks: { automatic: true }
+            }
+          })
+          window.MathJax.Hub.Queue([
+            'Typeset',
+            window.MathJax.Hub,
+            this.$refs.mathJaxEl
+          ])
+        }
+      }
+    },
+    mounted() {
+      this.renderMathJax()
+    }
+  }
+</script>
--- app/src/views/ConvolutionalEncoder.vue
+++ app/src/views/ConvolutionalEncoder.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="convolutional-encoder">
-    insert encoder here
+    polynomial: <MathField :data="'$g_0s_0 + g_1s_1x$'" />
   </div>
 </template>
 
 <script lang="ts">
-export default {
+import MathField from '@/components/AppMathJax.vue';
 
-}
+export default {
+  components: { MathField },
+};
 </script>
 
 <style scoped lang="less">
